99eda3d584ab1cf69106e19779656add2340e639
Choi-jeonghoon <jeongssi94@gmail.com>
2024-04-01 18:35:42 +0900

    init

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -16,3 +16,16 @@ export class ApiService {
         throw new Error("에러임");
     }
 }
+
+/**
+ * @TODO
+ * 1. API 키 발급받고 .env 작성
+ * 2. 환경변수 api.service.ts 에서 불러오기
+ * 3. 닉네임 검색하는 로직 작성
+ * 4. 컨트롤러 작성하기 전에 TDD 방식이니 테스트코드 먼저 작성
+ * 5. 올 패스 받으면 컨트롤러 작성
+ * 6. POSTMAN으로 최종 테스트
+ * 
+ * 7. 그러고 나서 process.service 작성
+ * ~~ 반복
+ */
