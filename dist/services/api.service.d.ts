a55d9e22f41ebdf03ec952a250a9eb7440d6af8a
SJ <80312906+SeongJae-git@users.noreply.github.com>
2024-04-02 13:25:09 +0900

    Delete dist directory

--- dist/services/api.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { HttpService } from '@nestjs/axios';
-export declare class ApiService {
-    private readonly httpService;
-    private readonly riotApiUrl;
-    private readonly riotApiKey;
-    constructor(httpService: HttpService);
-    search(nickname: string): Promise<object>;
-    beError(): Promise<any>;
-}
